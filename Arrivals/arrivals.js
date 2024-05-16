3cc36d88e6fe6403cd0e837fbd0055d437525171
Sujal Jagdev <rg2844537@gmail.com>
2024-05-16 15:03:17 +0530

    "updated"

--- Arrivals/arrivals.js
+++ Arrivals/arrivals.js
@@ -1,4 +1,7 @@
-fetch('http://localhost:3000/Arrivals_Data').then((res)=>res.json()).then((data)=>makeCards(data))
+let filterData = []
+fetch('http://localhost:3000/Arrivals_Data').then((res)=>res.json()).then((data)=>{makeCards(data)
+filterData = data
+})
 
 function makeCards(data){
     let store = data.map((el)=>singleCard(el.title,el.image_url,el.price))
@@ -8,7 +11,7 @@ function makeCards(data){
 function singleCard(title,images,price){
     let card = `
     <div
-                class="cart-item col-lg-3 col-md-6 col-sm-6 col-12 p-lg-3 p-md-3 p-sm-4 ps-5"
+                class="cart-item col-lg-3 col-md-5 col-sm-5 col-12 p-lg-3 p-md-3 p-sm-4 ps-5"
               >
                 <img
                   src=${images}
@@ -22,3 +25,23 @@ function singleCard(title,images,price){
 
     return card;
 }
+
+document.querySelector('#Backpacks').addEventListener('click',()=>{
+ let store = filterData.filter((el)=>el.category == "Backpack")
+ makeCards(store)
+})
+
+document.querySelector('#Duffles').addEventListener('click',()=>{
+  let store = filterData.filter((el)=>el.category == "duffle")
+  makeCards(store)
+ })
+
+ document.querySelector('#Hard-Shells').addEventListener('click',()=>{
+  let store = filterData.filter((el)=>el.category == "Hard Shells")
+  makeCards(store)
+ })
+
+ document.querySelector('#Hard-Shells').addEventListener('click',()=>{
+  let store = filterData.filter((el)=>el.category == "Hard Shells")
+  makeCards(store)
+ })
