fee9a0a51285ed3708d71e3fa8eb328db84dd5de
Sujal Jagdev <rg2844537@gmail.com>
2024-05-20 15:16:25 +0530

    "updated"

--- Arrivals/arrivals.js
+++ Arrivals/arrivals.js
@@ -1,17 +1,20 @@
-let filterData = []
-fetch('http://localhost:3000/Arrivals_Data').then((res) => res.json()).then((data) => {
-  makeCards(data)
-  filterData = data
-})
+let filterData = [];
+fetch("http://localhost:3000/Arrivals_Data")
+  .then((res) => res.json())
+  .then((data) => {
+    makeCards(data);
+    filterData = data;
+  });
 
 function makeCards(data) {
-  let store = data.map((el) => singleCard(el.title, el.image_url, el.price))
-  document.querySelector('.arrMainBar-column').innerHTML = store;
+  let store = data.map((el) => singleCard(el.title, el.image_url, el.price));
+  document.querySelector(".arrMainBar-column").innerHTML = store;
 }
 
 function singleCard(title, images, price) {
   let card = `
-    <div
+ 
+    <a href="/Add To Cart/Add.html"
                 class="cart-item col-lg-3 col-md-5 col-sm-5 col-12 p-lg-3 p-md-3 p-sm-4 ps-5"
               >
                 <img
@@ -21,64 +24,83 @@ function singleCard(title, images, price) {
                 />
                 <p id="cart-item-name" class="mt-2 lh-sm">${title}</p>
                 <p id="cart-item-price">Price: $${price}</p>
-              </div>
-    `
+              </a>
+            
+    `;
 
   return card;
 }
 
-document.querySelector('.Backpacks',).addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Backpack")
-  makeCards(store)
-})
+// Filters
+document.querySelector(".Backpacks").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Backpack");
+  makeCards(store);
+});
+document.querySelector(".Duffles").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "duffle");
+  makeCards(store);
+});
+document.querySelector(".Hard-Shells").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Hard Shells");
+  makeCards(store);
+});
+document.querySelector(".Hard-Shells").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Hard Shells");
+  makeCards(store);
+});
+document.querySelector(".Hip-Packs").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Hip Packs");
+  makeCards(store);
+});
+document.querySelector(".Organizers").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "organizers");
+  makeCards(store);
+});
+document.querySelector(".Soft-Shells").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Soft shell");
+  makeCards(store);
+});
+document.querySelector(".Totes").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Totes");
+  makeCards(store);
+});
 
-document.querySelector('.Duffles').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "duffle")
-  makeCards(store)
-})
-
-document.querySelector('.Hard-Shells').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Hard Shells")
-  makeCards(store)
-})
-
-document.querySelector('.Hard-Shells').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Hard Shells")
-  makeCards(store)
-})
-
-document.querySelector('.Hip-Packs').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Hip Packs")
-  makeCards(store)
-})
-
-document.querySelector('.Organizers').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "organizers")
-  makeCards(store)
-})
-
-document.querySelector('.Soft-Shells').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Soft shell")
-  makeCards(store)
-})
-
-document.querySelector('.Totes').addEventListener('click', () => {
-  let store = filterData.filter((el) => el.category == "Totes")
-  makeCards(store)
-})
-
-document.querySelector('#HL').addEventListener('click', () => {
-  const my = filterData.sort((a, b) => b.price - a.price)
+// High And Loo Filter
+document.querySelector("#HL").addEventListener("click", () => {
+  const my = filterData.sort((a, b) => b.price - a.price);
   makeCards(my);
-})
-
-document.querySelector('#LH').addEventListener('click', () => {
-  const my = filterData.sort((a, b) => a.price - b.price)
+});
+document.querySelector("#LH").addEventListener("click", () => {
+  const my = filterData.sort((a, b) => a.price - b.price);
   makeCards(my);
-})
+});
 
-
-document.querySelector('#Backpacks').addEventListener('click',()=>{
-  let store = filterData.filter((el) => el.category == "Backpack")
-  makeCards(store)
-})
+// Responsive Filters
+document.querySelector("#Backpacks").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Backpack");
+  makeCards(store);
+});
+document.querySelector("#Duffles").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "duffle");
+  makeCards(store);
+});
+document.querySelector("#Hard-Shells").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Hard Shells");
+  makeCards(store);
+});
+document.querySelector("#Hip-Packs").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Hip Packs");
+  makeCards(store);
+});
+document.querySelector("#Organizers").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "organizers");
+  makeCards(store);
+});
+document.querySelector("#Soft-Shells").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Soft shell");
+  makeCards(store);
+});
+document.querySelector("#Totes").addEventListener("click", () => {
+  let store = filterData.filter((el) => el.category == "Totes");
+  makeCards(store);
+});
